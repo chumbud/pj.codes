f0485652d52022d11ca1dbdf6079768823bfaa00
PJ Rosa <demotednyk@gmail.com>
2017-10-16 20:03:31 -0400

    figured i should start using git now before something bad happens

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+require 'em/pure_ruby'
+
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'tzinfo-data', platforms: [:x64_mingw]
+gem 'wdm', '~> 0.1', platforms: [:x64_mingw]
+gem 'middleman-livereload'
---
+++ source/javascripts/functions.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$('.info a').hover(function() {
+		$(this).closest('.project-list-item').find('.thumbnail a').addClass('hover');
+	}, function() {
+		$(this).closest('.project-list-item').find('.thumbnail a').removeClass('hover');
+	});
+
+	$('.thumbnail a').hover(function() {
+		$(this).closest('.project-list-item').find('.info a').addClass('hover');
+		console.log("hi");
+	}, function() {
+		$(this).closest('.project-list-item').find('.info a').removeClass('hover');
+	});
+
+	$('.read-more').on('click', function(e) {
+		e.preventDefault();
+		if($('.read-more-section').hasClass('open')) {
+			$('.read-more').text('read more');
+		} else {
+			$(this).text('actually, pls stop');
+		}
+
+		$('.read-more-section').toggleClass('open');
+	});
+	$('.article-gallery-image').on('click', function() {
+		if(!$(this).hasClass('expanded')) {
+			$(this).addClass('expanded');
+		} else {
+			$('.article-gallery-image').removeClass('expanded');
+			$(this).addClass('expanded');
+		}
+	});
+});
